77047b014ef0fd65a161e34b7a8a8313dc1563af
wzh <837713748@qq.com>
2020-07-21 19:19:05 +0800

    部门排序

--- run/src/main/java/com/xr/run/RunApplication.java
+++ run/src/main/java/com/xr/run/RunApplication.java
@@ -17,7 +17,7 @@ import java.util.Map;
 
 @SpringBootApplication
 @MapperScan("com.xr.run.dao")
-public class RunApplication extends SpringBootServletInitializer{
+public class RunApplication {
     @Resource
     private Environment environment;
     public static void main(String[] args) {
@@ -34,10 +34,10 @@ public class RunApplication extends SpringBootServletInitializer{
         }
     }
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        // 注意这里要指向原先用main方法执行的Application启动类
-        return builder.sources(RunApplication.class);
-    }
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//        // 注意这里要指向原先用main方法执行的Application启动类extends SpringBootServletInitializer
+//        return builder.sources(RunApplication.class);
+//    }
 
 }
