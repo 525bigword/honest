1f6ccfe99ea5bfd16e8eadf7e6537887a8f3c6b1
mpy <2423615474@qq.com>
2020-07-23 21:36:54 +0800

    mpy 7.23 21

--- run/src/main/java/com/xr/run/RunApplication.java
+++ run/src/main/java/com/xr/run/RunApplication.java
@@ -34,11 +34,11 @@ public class RunApplication {
             viewResolver.setStaticVariables(vars);
         }
     }
-//
-//    @Override
-//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-//        // 注意这里要指向原先用main方法执行的Application启动类extends SpringBootServletInitializer
-//        return builder.sources(RunApplication.class);
-//    }
+
+    /*@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 注意这里要指向原先用main方法执行的Application启动类
+        return builder.sources(RunApplication.class);extends SpringBootServletInitializer
+    }*/
 
 }
